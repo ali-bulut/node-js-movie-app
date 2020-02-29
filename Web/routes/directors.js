054573925c32123bfb4b479c45eae3ec636fd085
Ali Bulut <alibulut@yahoo.com>
2020-02-29 18:36:37 +0300

    getting director details operation was created

--- Web/routes/directors.js
+++ Web/routes/directors.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose=require('mongoose');
+
 //Models
 const Director = require('../models/Director');
 
@@ -28,6 +30,58 @@ router.get('/', (req, res) => {
         path: '$movies',
         //eğer bir yönetmenin henüz filmi olmasa bile sayfaya yazdırır.
         //yani join işleminde movies'te herhangi bir datayla eşleşmese bile tüm directors tablosunu sayfaya yazdırır.
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies:{
+          $push: '$movies' 
+        }
+      }
+    },
+    {
+      $project:{
+        _id: '$_id._id', 
+        name:'$_id.name',
+        surname:'$_id.surname',
+        bio:'$_id.bio',
+        movies:'$movies'
+      }
+    }
+  ])
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+})
+
+router.get('/:director_id', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $match:{
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
         preserveNullAndEmptyArrays: true
       }
     },
