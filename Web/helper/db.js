08824f2544f616bbabe48c2cbc29465c0a3fd774
Ali Bulut <alibulut@yahoo.com>
2020-02-29 15:50:06 +0300

    adding movies operation was created on movies endpoint

--- Web/helper/db.js
+++ Web/helper/db.js
@@ -1,11 +1,14 @@
 const mongoose=require('mongoose');
 
 module.exports=()=>{
-    mongoose.connect('mongodb://alibulut:12345a@ds221990.mlab.com:21990/heroku_4vh0cqv0', {useMongoClient:true});
+    mongoose.connect('mongodb://alibulut:12345a@ds221990.mlab.com:21990/heroku_4vh0cqv0', {useUnifiedTopology:true, useNewUrlParser:true});
     mongoose.connection.on('open',()=>{
         console.log('MongoDB: Connected');
     })
     mongoose.connection.on('error',(err)=>{
         console.log('MongoDB Connection Error: ',err);
     })
+
+    //promise yapısını projemize tanıttık.
+    mongoose.Promise=global.Promise;
 }
--- Web/routes/movies.js
+++ Web/routes/movies.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({status:1});
+//models
+const Movie=require('../models/Movie')
+
+router.post('/', (req, res, next) => {
+  //request ile gönderdiğimiz post datasını değişkene atadık.
+  //böyle ayrı ayrı yazmamızın sebebi direkt title yada category diye yazarak gelen dataya ulaşabiliriz.
+  //direkt const data da yazsak olurdu. data.title yada data.category şeklinde de ulaşabilirdik.
+
+  //yada hiç böyle kasmadan;
+  //const { title, imdb_score, category, country, year }=req.body;
+
+  //direkt olarak bu şekilde de datayı movie nesnesine çekebiliriz.
+  const movie=new Movie(req.body);
+
+  //sol taraftaki Movie modelinden gelen değerler sağ taraftaki yukarıda req.body'den çektiğimiz değerler
+  //post datasını çekip Movie modeline sırayla eşitledik.
+  // const movie=new Movie({
+  //   title:title,
+  //   imdb_score:imdb_score,
+  //   category:category,
+  //   country:country,
+  //   year:year
+  // })
+
+  //save ile db'ye gelen datayı kaydettik.
+  // movie.save((err,data)=>{
+  //   if(err)
+  //     res.json(err);
+
+  //     //status:1 demek kayıt başarılı demek (kendi gözümüzde. normalde böyle bir kullanım yok)
+  //   res.json({status:1});
+  // })
+
+  //yukarıda yaptığımız şeyle bu yaptığımız tamamiyle aynı. 
+  //amacımız promise yapısını kullanarak daha temiz bir kod yazmaktır.
+  const promise=movie.save();
+
+  promise
+  .then((data)=>{
+     res.json({status:1});
+  })
+  .catch((err)=>{
+    res.json(err);
+  })
+
 });
 
 module.exports = router;
