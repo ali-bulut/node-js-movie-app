5307be3ff1a751a9e8a78d70f1e47f3155ca1fed
Ali Bulut <alibulut@yahoo.com>
2020-02-29 18:58:02 +0300

    validation operations were done

--- Web/models/Movie.js
+++ Web/models/Movie.js
@@ -1,16 +1,40 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
+
+const currentTime = new Date()
+
+
 const MovieSchema=new Schema({
     director_id:Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true
+        required:[true, '`{PATH}` field is required!'],
+        maxlength:15,
+        minlength:2
     },
-    category:String,
-    country:String,
-    year:Number,
-    imdb_score:Number,
+    category:{
+        type:String,
+        required:true,
+        maxlength:15,
+        minlength:1
+    },
+    country:{
+        type:String,
+        required:true,
+        maxlength:10,
+        minlength:1
+    },
+    year:{
+        type:Number,
+        max:currentTime.getUTCFullYear(),
+        min:1900
+    },
+    imdb_score:{
+        type:Number,
+        max:10,
+        min:0
+    },
     createdAt:{
         type:Date,
         default:Date.now
