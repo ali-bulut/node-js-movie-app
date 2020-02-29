d529c05060ea343de3bf795e0cc4101fbeecf0d0
Ali Bulut <alibulut@yahoo.com>
2020-02-29 18:06:17 +0300

    listing directors and directors' movies operation was created

--- Web/routes/directors.js
+++ Web/routes/directors.js
@@ -2,16 +2,64 @@ const express = require('express');
 const router = express.Router();
 
 //Models
-const Director=require('../models/Director');
+const Director = require('../models/Director');
 
 router.post('/', (req, res, next) => {
-  const director=new Director(req.body);
-  const promise=director.save();
-  promise.then((data)=>{
+  const director = new Director(req.body);
+  const promise = director.save();
+  promise.then((data) => {
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   })
 });
+
+router.get('/', (req, res) => {
+  const promise = Director.aggregate([{
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        //eğer bir yönetmenin henüz filmi olmasa bile sayfaya yazdırır.
+        //yani join işleminde movies'te herhangi bir datayla eşleşmese bile tüm directors tablosunu sayfaya yazdırır.
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies:{
+          $push: '$movies' 
+        }
+      }
+    },
+    {
+      $project:{
+        _id: '$_id._id', 
+        name:'$_id.name',
+        surname:'$_id.surname',
+        bio:'$_id.bio',
+        movies:'$movies'
+      }
+    }
+  ])
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+})
 
 module.exports = router;
