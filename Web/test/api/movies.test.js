4bfb6207daeefe239a46d13ea5ac36c4cfb3e030
Ali Bulut <alibulut@yahoo.com>
2020-03-01 15:40:15 +0300

    unit testing operations were created for adding movie endpoint by using mocha and chai

--- Web/test/api/movies.test.js
+++ Web/test/api/movies.test.js
@@ -28,4 +28,29 @@ describe('/api/movies tests', ()=>{
             })
         })
     })
+
+    describe('/POST movies',()=>{
+        it('it should post a movie', (done)=>{
+            const movie={
+                title:"Udemy",
+                director_id:"5e5a7853da1cc3730c16e972",
+                category:"Comedy",
+                country:"Turkey",
+                year:1950,
+                imdb_score:8.2
+            }
+            chai.request(server).post('/api/movies').send(movie).set('x-access-token',token).end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('imdb_score');
+
+                done();
+            })
+        })
+    })
 })
